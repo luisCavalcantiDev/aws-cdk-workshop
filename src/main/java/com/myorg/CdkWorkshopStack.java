ef1538e60f925f927c053cc541a2917d64fbf068
luis.cavalcanti583@gmail.com <luisdev@luis-dev-pc-2.localdomain>
2022-06-05 10:25:33 -0300

    Hello CDK feature

--- src/main/java/com/myorg/CdkWorkshopStack.java
+++ src/main/java/com/myorg/CdkWorkshopStack.java
@@ -1,12 +1,16 @@
 package com.myorg;
 
-import software.constructs.Construct;
 import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.sns.Topic;
 import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
 import software.amazon.awscdk.services.sqs.Queue;
+import software.constructs.Construct;
 
 public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id) {
@@ -16,13 +20,30 @@ public class CdkWorkshopStack extends Stack {
     public CdkWorkshopStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
+        // buildQueues();
+
+        //Defines Lambda function resource
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+                .runtime(Runtime.NODEJS_14_X)
+                .code(Code.fromAsset("lambda"))
+                .handler("hello.handler")
+                .build();
+                
+        //Defines an API Gateway REST API resource based by "hello" lambda function 
+        LambdaRestApi.Builder.create(this, "Endpoint")
+                .handler(hello)
+                .build();
+    }
+
+    //Defines SQS queue and SNS queue resources and add to SNS queue subscriptions.
+    private void buildQueues() {
         final Queue queue = Queue.Builder.create(this, "CdkWorkshopQueue")
                 .visibilityTimeout(Duration.seconds(300))
                 .build();
 
         final Topic topic = Topic.Builder.create(this, "CdkWorkshopTopic")
-            .displayName("My First Topic Yeah")
-            .build();
+                .displayName("My First Topic Yeah")
+                .build();
 
         topic.addSubscription(new SqsSubscription(queue));
     }
--- src/main/java/com/myorg/Config.java
+++ src/main/java/com/myorg/Config.java
@@ -2,8 +2,8 @@ package com.myorg;
 
 public class Config {
     //tests
-    public static final String ACCOUNT_ID = "";
-    public static final String REGION_ID = "";
+    public static final String ACCOUNT_ID = "039105259950";
+    public static final String REGION_ID = "us-east-1";
 
     //credentials
     //define your own profile on your environment: 
