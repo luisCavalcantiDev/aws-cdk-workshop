d68b0646afc0b36a8dfbed53f7ee82dfb104d0ea
luis.cavalcanti583@gmail.com <luisdev@luis-dev-pc-2.localdomain>
2022-06-05 13:11:01 -0300

    Step build Pipeline

--- src/main/java/com/myorg/WorkshopPipelineStack.java
+++ src/main/java/com/myorg/WorkshopPipelineStack.java
@@ -1,7 +1,12 @@
 package com.myorg;
 
+import java.util.List;
+
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.pipelines.CodeBuildStep;
+import software.amazon.awscdk.pipelines.CodePipeline;
+import software.amazon.awscdk.pipelines.CodePipelineSource;
 import software.amazon.awscdk.services.codecommit.Repository;
 import software.constructs.Construct;
 
@@ -13,9 +18,21 @@ public class WorkshopPipelineStack extends Stack {
     public WorkshopPipelineStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
 
-        //Defines CodeCommit repository resource
-        Repository.Builder.create(this, "WorkshopRepo")
-            .repositoryName("WorkshopRepo")
-            .build();
+        // Defines CodeCommit repository resource
+        Repository repo = Repository.Builder.create(this, "WorkshopRepo")
+                .repositoryName("WorkshopRepo")
+                .build();
+
+        CodePipeline.Builder.create(this, "Pipeline")
+                .pipelineName("WorkshopPipeline")
+                .synth(CodeBuildStep.Builder.create("SynthStep")
+                        .input(CodePipelineSource.codeCommit(repo, "master"))
+                        .installCommands(List.of(
+                                "npm install -g aws-cdk"))
+                        .commands(List.of(
+                                "mvn package",
+                                "npx cdk synth"))
+                        .build())
+                .build();
     }
 }
